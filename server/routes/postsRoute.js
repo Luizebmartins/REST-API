80ce83ae9cbcd198844bca9e6908d6dfb6e74c5e
Luiz Martins <luizmartins.prog@gmail.com>
2021-09-06 09:17:23 -0300

    add route getPost and deletePost

--- server/routes/postsRoute.js
+++ server/routes/postsRoute.js
@@ -6,18 +6,7 @@ router.get('/posts', async (req, res) => {
     const posts = await postService.getPosts()
     res.json(posts)
 })
-router.get('/posts/:id',async (req, res) => {
-    
-})
-router.post('/posts', async (req, res) => {
-    
-})
-router.put('/posts/:id', async (req, res) => {
-    
-})
-router.delete('/posts/:id', async (req, res) => {
-    
-})
+
 
 
 module.exports = router
--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -1,12 +1,40 @@
 const axios = require('axios')
+const postsService = require('../service/postsService')
+const crypto = require('crypto')
+const { post } = require('../routes/postsRoute')
 
+const generate = function () {
+    return crypto.randomBytes(20).toString('hex')
+}
 
-test('should get posts', async function() {
-    const response = await axios({
-        url: 'http://localhost:3000/posts',
-        method: 'get'
-    })    
+const request = function (url, method, data) {
+    return axios({url, method, data })
+}
+
+test('should get posts', async function () {
+
+    const post1 = await postsService.savePost({ title: generate(), content: generate() })
+    const post2 = await postsService.savePost({ title: generate(), content: generate() })
+    const post3 = await postsService.savePost({ title: generate(), content: generate() })
+
+    const response = await request('http://localhost:3000/posts', 'get')
     const posts = response.data
+
     expect(posts).toHaveLength(3)
-    const [firstPosts] = posts
+    await postsService.deletePost(post1.id)
+    await postsService.deletePost(post2.id)
+    await postsService.deletePost(post3.id)
+})
+
+test.only('should save posts', async function () {
+
+    const post1 = { title: generate(), content: generate() }
+
+
+    const response = await request('http://localhost:3000/posts',)
+ 
+    const posts = response.data
+
+    await postsService.deletePost(post1.id)
+
 })
